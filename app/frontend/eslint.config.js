a5861d722104c1b882a868284391f672a10c40fe
harideveloper <hariprasad.sundharesan@gmail.com>
2025-11-11 10:53:50 +0000

    linter ci

--- app/frontend/eslint.config.js
+++ app/frontend/eslint.config.js
@@ -28,7 +28,7 @@ export default [
       },
     },
     rules: {
-      'no-unused-vars': 'warn',
+      'no-unused-vars': 'off',
       'no-console': 'off',
       'react/prop-types': 'off',
     },
