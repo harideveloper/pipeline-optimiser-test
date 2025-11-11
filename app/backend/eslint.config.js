d0e4287444cb1fda6c37befed382df985691c750
harideveloper <hariprasad.sundharesan@gmail.com>
2025-11-11 10:29:04 +0000

    Adding more  ci test files

---
+++ app/backend/eslint.config.js
@@ -0,0 +1,16 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: "latest",
+      sourceType: "commonjs",
+    },
+    rules: {
+      "no-unused-vars": "warn",
+    },
+  },
+];
